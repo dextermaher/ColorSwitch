0178f07f28e349d4650218f9d33cd6e68b444c55
Dexter Maher <dexter@maher-family.net>
2021-03-18 17:25:25 -0700

    added some stuff

--- ColorSwitch/Scenes/GameScene.swift
+++ ColorSwitch/Scenes/GameScene.swift
@@ -35,50 +35,11 @@ class GameScene: SKScene {
             layoutScene()
     }
     
-    func setUpPhysics() {
-        physicsWorld.gravity = CGVector(dx: 0.0, dy: -1.9)
-        physicsWorld.contactDelegate = self
-    }
     
-    func layoutScene() {
-        backgroundColor = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0)
-        
-        colorSwitch = SKSpriteNode(imageNamed: "ColorCircle")
-        colorSwitch.size = CGSize(width: frame.size.width/3, height: frame.size.width/3)
-        colorSwitch.position = CGPoint(x: frame.midX, y: frame.minY + colorSwitch.size.height)
-        colorSwitch.zPosition = ZPositions.colorSwitch
-        colorSwitch.physicsBody = SKPhysicsBody(circleOfRadius: colorSwitch.size.width/2)
-        colorSwitch.physicsBody?.categoryBitMask = PhysicsCategories.switchCategory
-        colorSwitch.physicsBody?.isDynamic = false
-        addChild(colorSwitch)
-        
-        scoreLabel.fontName = "AvenirNext-Bold"
-        scoreLabel.fontSize = 60.0
-        scoreLabel.fontColor = UIColor.white
-        scoreLabel.position = CGPoint(x: frame.midX, y: frame.midY)
-        scoreLabel.zPosition = ZPositions.label
-        addChild(scoreLabel)
-        
-        spawnBall()
-    }
+    // MARK: UPDATE ITEMS
     
     func updateScoreLabel() {
         scoreLabel.text = "\(score)"
-    }
-    
-    func spawnBall() {
-        currentColorIndex = Int(arc4random_uniform(UInt32(4)))
-        
-        let ball = SKSpriteNode(texture: SKTexture(imageNamed: "ball"), color: PlayColors.colors[currentColorIndex!], size: CGSize(width: 30.0, height: 30.0))
-        ball.colorBlendFactor = 1.0
-        ball.name = "Ball"
-        ball.position = CGPoint(x: frame.midX, y: frame.maxY)
-        ball.zPosition = ZPositions.ball
-        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2)
-        ball.physicsBody?.categoryBitMask = PhysicsCategories.ballCategory
-        ball.physicsBody?.contactTestBitMask = PhysicsCategories.switchCategory
-        ball.physicsBody?.collisionBitMask = PhysicsCategories.none
-        addChild(ball)
     }
     
     func turnWheel() {
